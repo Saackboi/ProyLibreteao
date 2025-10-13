b03b2366c6c525a53f4f0b568d1a24ff2726bbde
rodolfs <davidrenecortezsalazar@gmail.com>
2025-10-12 19:58:01 -0500

    Agregando interfas de vista catalogo como invitado

---
+++ Interface_invitados.vb
@@ -0,0 +1,33 @@
+﻿Public Class Interface_invitados
+    Private Sub lblTitulo_Click(sender As Object, e As EventArgs) Handles lblTitulo.Click
+
+    End Sub
+
+
+
+    Private Sub pnlIzquierdo_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Interface_invitados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+
+    Private Sub Interface_invitados_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+
+
+
+    End Sub
+
+    Private Sub plLibro_Paint(sender As Object, e As PaintEventArgs) Handles plLibro.Paint
+        Dim colorBorde As Color = Color.Black ' Borde negro
+        Dim grosor As Integer = 2            ' Grosor del borde
+        Dim rect As New Rectangle(0, 0, plLibro.Width - 1, plLibro.Height - 1)
+
+        Using pen As New Pen(colorBorde, grosor)
+            e.Graphics.DrawRectangle(pen, rect)
+        End Using
+    End Sub
+End Class
