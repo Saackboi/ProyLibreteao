7a0a536154d5f9a912c9a35a6a5c2ad43c78bef7
Elizabeth-Ha-code <elizabeth.h.almillategui@gmail.com>
2025-10-14 23:29:07 -0500

    Anexo  de direccion de  imagenes

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProyLibreteao.Interface_invitados
+            Me.MainForm = Global.ProyLibreteao.Formulario_Bibliotecario
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
